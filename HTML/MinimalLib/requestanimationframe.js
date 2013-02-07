beadab41dc9bfe866209d50187b41ed1d90b8870
idettman <idettman@gmail.com>
2013-02-07 10:52:24 -0800

    Updated with Opera engineer Erik Möller's Request Animation Frame Polyfill

--- HTML/MinimalLib/requestanimationframe.js
+++ HTML/MinimalLib/requestanimationframe.js
@@ -1,35 +1,70 @@
-// Paul Irish's: Request Animation Frame - Shim Implementation
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// Request Animation Frame Polyfill
 
+(function() {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+		window.cancelAnimationFrame =
+			window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+	}
 
-// shim layer with setTimeout fallback
-window.requestAnimFrame = (function ()
-{
-	return  window.requestAnimationFrame ||
-		window.webkitRequestAnimationFrame ||
-		window.mozRequestAnimationFrame ||
-		window.oRequestAnimationFrame ||
-		window.msRequestAnimationFrame ||
-		function (callback)
-		{
-			window.setTimeout (callback, 1000 / 60);
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = function(callback, element) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+				timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
 		};
-}) ();
+
+	if (!window.cancelAnimationFrame)
+		window.cancelAnimationFrame = function(id) {
+			clearTimeout(id);
+		};
+}());
 
 
-// usage:
-// instead of setInterval(render, 16) ....
-/*(function animloop ()
-{
-	requestAnimFrame (animloop);
-	render ();
-}) ();
+// Example Code from Mr Doob
+/*
+ // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
-function render ()
-{
-	"use strict";
+ var canvas, context, toggle;
 
-}*/
+ init();
+ animate();
 
-// place the rAF *before* the render() to assure as close to
-// 60fps with the setTimeout fallback.
+ function init() {
+
+ canvas = document.createElement( 'canvas' );
+ canvas.width = 512;
+ canvas.height = 512;
+
+ context = canvas.getContext( '2d' );
+
+ document.body.appendChild( canvas );
+
+ }
+
+ function animate() {
+ requestAnimFrame( animate );
+ draw();
+
+ }
+
+ function draw() {
+
+ var time = new Date().getTime() * 0.002;
+ var x = Math.sin( time ) * 192 + 256;
+ var y = Math.cos( time * 0.9 ) * 192 + 256;
+ toggle = !toggle;
+
+ context.fillStyle = toggle ? 'rgb(200,200,20)' :  'rgb(20,20,200)';
+ context.beginPath();
+ context.arc( x, y, 10, 0, Math.PI * 2, true );
+ context.closePath();
+ context.fill();
+
+ }
+ */
