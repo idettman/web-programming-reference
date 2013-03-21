887ea22e0766a15e19bb368a50423bc3c6c0c55b
idettman <idettman@gmail.com>
2013-03-20 19:04:18 -0700

    Added missing file for last commit. Removed all personal info from auth_data.py so the example code is complete for the youtube video upload

---
+++ Python/Youtube_VideoUploadTest/auth_data.py
@@ -0,0 +1,10 @@
+#!/home/idettman/bin/python
+
+
+class AuthData:
+	ClientId = ''
+	ClientSecret = ''
+	Scope = 'https://www.googleapis.com/auth/youtube.upload'
+	RedirectUri = ''
+	StorageFileName = ''
+
