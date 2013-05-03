bbdfc63745203f21066df11a7417e491592b8ed1
idettman <idettman@gmail.com>
2013-05-02 19:15:11 -0700

    Setup jquery to handle layout resizing

--- MNSite/js/main.js
+++ MNSite/js/main.js
@@ -3,19 +3,30 @@ var Main = {
 
 	init: function ()
 	{
-		// Get reference to video play so it can be accessed within Main
-		/*this.reelVideoPlayer = $ ('#reelVideoPlayer').get (0);*/
-		this.reelVideoPlayer = _V_('reelVideoPlayer');
+		$.proxy (this.initVideoPlayer, this);
+		$.proxy (this.initPhotoViewer, this);
+		$.proxy (this.initPageSelector, this);
+		$.proxy (this.initLayout, this);
 
-		_V_('reelVideoPlayer').ready(function() {
-
-			//var myPlayer = this;
-			// EXAMPLE: Start playing the video.
-			//myPlayer.play();
-			console.log ('REEL VIDEO PLAYER READY!!!!!');
-		});
+		this.initVideoPlayer ();
+		this.initPhotoViewer ();
+		this.initPageSelector ();
+		this.initLayout ();
+	},
 
 
+	initVideoPlayer: function ()
+	{
+		this.reelVideoPlayer = _V_ ('reelVideoPlayer');
+		/*_V_('reelVideoPlayer').ready(function() {
+		 //var myPlayer = this; //myPlayer.play();
+		 console.log ('reelvideoplayer ready');
+		 });*/
+	},
+
+
+	initPhotoViewer: function ()
+	{
 		$ ('#photos').responsiveSlides ({
 			auto: false,
 			fade: 500,
@@ -25,27 +36,72 @@ var Main = {
 			nextText: 'Next',
 			manualControls: '#photos-pager'
 		});
-
-
-		$.hideAllExcept ('.tab', '.box', $.proxy(Main.onPageChange, Main));
 	},
 
 
-	onPageChange: function (pageID) {
+	initPageSelector: function ()
+	{
+		$.hideAllExcept ('.tab', '.box', $.proxy (this.onPageChange, this));
+	},
 
 
-		if (pageID !== '#reel') {
+	onPageChange: function (pageID)
+	{
+
+		console.log ('onPageChange:', this, this.reelVideoPlayer);
+
+		if (pageID !== '#reel')
+		{
 			this.reelVideoPlayer.pause ();
 		}
-		else {
-			if (this.reelVideoPlayer.currentTime() !== 0) {
+		else
+		{
+			if (this.reelVideoPlayer.currentTime () !== 0)
+			{
 				this.reelVideoPlayer.currentTime (0);
+				this.reelVideoPlayer.play ();
 			}
-			this.reelVideoPlayer.play ();
+		}
+	},
+
+
+	initLayout: function ()
+	{
+		$ ('#main').bind ('updateLayout', $.proxy(this.updateLayout, this));
+
+		$ (window).bind ('load resize orientationchange', function () {
+			$ ('#main').trigger ('updateLayout');
+		});
+
+		this.updateLayout ();
+	},
+
+
+	updateLayout: function ()
+	{
+//		console.log('body scroll height:', $('body')[0].scrollHeight);
+
+		var windowHeight = $(window).innerHeight();
+		var updatedHeight;
+
+		if (windowHeight > 240)
+		{
+			updatedHeight = windowHeight - $ ('#siteHeader').outerHeight () - 32;
+
+			$ ('#main').height (updatedHeight);
+			$ ('.pageContent').width ($ ('#main').width ());
+			$ ('.pageContent').height (updatedHeight);
+		}
+		else
+		{
+			updatedHeight = 240 - $ ('#siteHeader').outerHeight () - 24;
+
+			$ ('#main').height (updatedHeight);
+			$ ('.pageContent').width ($ ('#main').width ());
+			$ ('.pageContent').height (updatedHeight);
 		}
 	},
 
 
 	reelVideoPlayer: null
-
 };
