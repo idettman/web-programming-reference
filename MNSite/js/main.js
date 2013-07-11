1785a67bf3a4933d7aab0ab3f34ae7a571b114e4
idettman <idettman@gmail.com>
2013-07-11 06:52:42 -0700

    Resized images to 480 height

--- MNSite/js/main.js
+++ MNSite/js/main.js
@@ -1,7 +1,9 @@
 var Main = {
 
 	windowData: { windowWidth: 0,windowHeight: 0,isLandscapeView: false },
-
+	
+	videoData: { width: 854, height: 480},
+	
 	init: function () {
 		$.proxy (this.initVideoPlayer, this);
 		$.proxy (this.initPhotoViewer, this);
@@ -15,16 +17,12 @@ var Main = {
 	},
 
 	initVideoPlayer: function () {
-
-//		this.reelVideoPlayer = _V_ ('reelVideoPlayer');
-
 		var classLevelScope = this;
-
 		_V_ ('reelVideoPlayer').ready (function () {
 			classLevelScope.reelVideoPlayer = this;
 		});
 	},
-
+	
 	initPhotoViewer: function () {
 		$ ('#photos').responsiveSlides ({
 			auto: false,
@@ -76,24 +74,50 @@ var Main = {
 		this.windowData.windowWidth = $ (window).width ();
 		this.windowData.windowHeight = $ (window).height ();
 		this.windowData.isLandscapeView = (Math.max (this.windowData.windowWidth, this.windowData.windowHeight) === this.windowData.windowWidth);
-
+		
 		//var MIN_WIDTH = 30.0;//32.0
 		var MIN_WIDTH = 40.0;
 		var MAX_WIDTH = 64.2;
-
+		
+		
 		if (this.windowData.windowWidth / 16.0 < MAX_WIDTH) {
+
+			console.log ('width:', ((this.windowData.windowWidth / 16.0)/MAX_WIDTH));
+			
+			
+			
 			if (this.windowData.windowWidth / 16.0 > MIN_WIDTH) {
 				var emsWidth = this.windowData.windowWidth / 16.0;
+
+				
+				if (this.reelVideoPlayer) {
+					var scaleRatio = (this.windowData.windowWidth / 16.0)/MAX_WIDTH;
+					var videoWidth = Math.floor(this.windowData.windowWidth * scaleRatio);
+					var videoHeight = Math.floor(this.windowData.windowHeight * scaleRatio);
+					this.reelVideoPlayer.size (videoWidth, videoHeight);
+				}
+				
 				$ ("body").css ("font-size", (emsWidth / MAX_WIDTH) + "em");
 			}
 			else {
+
+				if (this.reelVideoPlayer) {
+					var scaleRatio = (MIN_WIDTH / 16.0)/MAX_WIDTH;
+					var videoWidth = Math.floor(this.windowData.windowWidth * scaleRatio);
+					var videoHeight = Math.floor(this.windowData.windowHeight * scaleRatio);
+					this.reelVideoPlayer.size (videoWidth, videoHeight);
+				}
+				
 				$ ("body").css ("font-size", MIN_WIDTH / MAX_WIDTH + "em");
 			}
 		}
 		else {
+			if (this.reelVideoPlayer) {
+				this.reelVideoPlayer.size(this.videoData.width, this.videoData.height);
+			}
 			$ ("body").css ("font-size", "1.0em");
 		}
 	},
-
+	
 	reelVideoPlayer: null
 };
